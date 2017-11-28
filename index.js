ae1618265ec930d9d684bbafa10cc905e23b1b7d
Frank van de Voorde <cairhiin@gmail.com>
2017-11-28 15:43:53 +0200

    Setting up Express server

---
+++ index.js
@@ -0,0 +1,49 @@
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+
+require('./lib/connection');
+let employees = require('./routes/books');
+let teams = require('./routes/authors');
+let publishers = require('./routes/publishers');
+let genres = require('./routes/genres');
+
+let app = express();
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(employees);
+app.use(teams);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+});
+
+module.exports = app;
