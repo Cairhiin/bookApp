0885fccba0e39393b6c29cbcf7e7ed0ac70e2b69
Frank van de Voorde <cairhiin@gmail.com>
2017-11-28 18:45:57 +0200

    Fixing errors
    Fixing errors

--- routes/authors.js
+++ routes/authors.js
@@ -3,14 +3,7 @@ var mongoose = require('mongoose');
 var Author = mongoose.model('Author');
 var router = express.Router();
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
 router.get('/authors', function (req, res, next) {
-=======
-router.get('/author', function (req, res, next) {
->>>>>>> origin/master
   Author.find().sort('name.last').exec(function (error, results) {
     if (error) {
       return next(error);
@@ -20,16 +13,9 @@ router.get('/author', function (req, res, next) {
   });
 });
 
-<<<<<<< HEAD
 router.get('/authors/:authorId', function (req, res, next) {
   Author.findOne({
-=======
-<<<<<<< HEAD
-=======
-router.get('/author/:authorId', function (req, res, next) {
->>>>>>> origin/master
   Team.findOne({
->>>>>>> origin/master
     _id: req.params.authorId
   }, function (error, results) {
     if (error) {
